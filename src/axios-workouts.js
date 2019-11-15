2ed35e58bdf314a85ff32892c45e80efcbd3c3e9
Tate Shepherd <tshepherd@kollecitve.com>
2019-11-15 15:55:42 -0800

    added secrets

--- src/axios-workouts.js
+++ src/axios-workouts.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import myURL from './secrets';
 
 const instance = axios.create({
-    baseURL: 'https://climbingproj.firebaseio.com'
+    baseURL: myURL
 });
 
 export default instance;

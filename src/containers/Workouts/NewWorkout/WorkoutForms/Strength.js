3342cdaa62e0837993bbee2de07e9f3f5a26bba5
Tate Shepherd <tshepherd@kollecitve.com>
2019-11-21 14:13:49 -0800

    added date picker

--- src/containers/Workouts/NewWorkout/WorkoutForms/Strength.js
+++ src/containers/Workouts/NewWorkout/WorkoutForms/Strength.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+  } from '@material-ui/pickers';
 
 const useStyles = makeStyles(theme => ({
     container: {
       display: 'flex',
-      flexWrap: 'wrap',
+      flexWrap: 'no-wrap',
+      flexDirection: 'column'
     },
     textField: {
       marginRight: '10px',
@@ -14,19 +20,37 @@ const useStyles = makeStyles(theme => ({
     },
   }));
 
-
  
-  export default function BaseFitness(props) {
+  export default function Strength(props) {
     const classes = useStyles();
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2019-08-18T21:11:54'));
+
+    const handleDateChange = date => {
+      setSelectedDate(date);
+    };
 
     return (
         <>
             <h3>New {props.workoutType} Workout</h3>
             <form className={classes.container}>
-              
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                disableToolbar
+                variant="inline"
+                format="MM/dd/yyyy"
+                margin="normal"
+                id="date-picker-inline"
+                label="Date"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                }}
+                />
+                </MuiPickersUtilsProvider>
                 <div><TextField className={classes.textField} onChange={(event) => props.titleChange(event)} id="title" type="text" placeholder='title'/></div>
                 <div><TextField className={classes.textField} multiline id="warmup" type="textarea" placeholder="test warm up" /></div>
-                <div><TextField className={classes.textField} id="description" type="textarea" placeholder="test description" /></div>
+                <div><TextField className={classes.textField} multiline id="comments" type="textarea" placeholder="test comments" /></div>
 
             </form>
         </>
